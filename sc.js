3f791213bb50b0491bcc79618e3e3b6ef8feb121
khushigavate <error404khushi@gmail.com>
2024-07-17 17:11:45 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,48 @@
+(function countdownTimer() {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+
+    // I'm adding this section so I don't have to keep updating this pen every year :-)
+    // Remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "07/22/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    // End
+    
+    const countDown = new Date(birthday).getTime(),
+          x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / day);
+          document.getElementById("hours").innerText = Math.floor((distance % day) / hour);
+          document.getElementById("minutes").innerText = Math.floor((distance % hour) / minute);
+          document.getElementById("seconds").innerText = Math.floor((distance % minute) / second);
+  
+          // Do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+            // Redirect to the main index page
+            window.location.href = "index44.html";
+          }
+          // Seconds
+        }, 0);
+})();
+
+// Run this function first if the counter is not running
+countdownTimer();
---
+++ script.js
@@ -0,0 +1,269 @@
+// Author: Hoang Tran (https://fb.com/99.hoangtran)
+
+// Github verson (1 file .html): https://github.com/HoangTran0410/3DCarousel/blob/master/index.html
+
+// Give me a coffee <3 https://github.com/HoangTran0410/HoangTran0410/blob/main/DONATE.md
+
+
+// You can change global variables here:
+var radius = 240; // how big of the radius
+var autoRotate = true; // auto rotate or not
+var rotateSpeed = -60; // unit: seconds/360 degrees
+var imgWidth = 120; // width of images (unit: px)
+var imgHeight = 170; // height of images (unit: px)
+
+// Link of background music - set 'null' if you dont want to play background music
+var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicControls = true; // Show UI music control
+
+/*
+     NOTE:
+       + imgWidth, imgHeight will work for video
+       + if imgWidth, imgHeight too small, play/pause button in <video> will be hidden
+       + Music link are taken from: https://hoangtran0410.github.io/Visualyze-design-your-own-/?theme=HauMaster&playlist=1&song=1&background=28
+       + Custom from code in tiktok video  https://www.facebook.com/J2TEAM.ManhTuan/videos/1353367338135935/
+*/
+
+
+// ===================== start =======================
+// animation start after 1000 miliseconds
+setTimeout(init, 1000);
+
+var odrag = document.getElementById('drag-container');
+var ospin = document.getElementById('spin-container');
+var aImg = ospin.getElementsByTagName('img');
+var aVid = ospin.getElementsByTagName('video');
+var aEle = [...aImg, ...aVid]; // combine 2 arrays
+
+// Size of images
+ospin.style.width = imgWidth + "px";
+ospin.style.height = imgHeight + "px";
+
+// Size of ground - depend on radius
+var ground = document.getElementById('ground');
+ground.style.width = radius * 3 + "px";
+ground.style.height = radius * 3 + "px";
+
+function init(delayTime) {
+  for (var i = 0; i < aEle.length; i++) {
+    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+    aEle[i].style.transition = "transform 1s";
+    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
+  }
+}
+
+function applyTranform(obj) {
+  // Constrain the angle of camera (between 0 and 180)
+  if(tY > 180) tY = 180;
+  if(tY < 0) tY = 0;
+
+  // Apply the angle
+  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+}
+
+function playSpin(yes) {
+  ospin.style.animationPlayState = (yes?'running':'paused');
+}
+
+var sX, sY, nX, nY, desX = 0,
+    desY = 0,
+    tX = 0,
+    tY = 10;
+
+// auto spin
+if (autoRotate) {
+  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+}
+
+// add background music
+if (bgMusicURL) {
+  document.getElementById('music-container').innerHTML += `
+<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+<p>If you are reading this, it is because your browser does not support the audio element.</p>
+</audio>
+`;
+}
+
+// setup events
+document.onpointerdown = function (e) {
+  clearInterval(odrag.timer);
+  e = e || window.event;
+  var sX = e.clientX,
+      sY = e.clientY;
+
+  this.onpointermove = function (e) {
+    e = e || window.event;
+    var nX = e.clientX,
+        nY = e.clientY;
+    desX = nX - sX;
+    desY = nY - sY;
+    tX += desX * 0.1;
+    tY += desY * 0.1;
+    applyTranform(odrag);
+    sX = nX;
+    sY = nY;
+  };
+
+  this.onpointerup = function (e) {
+    odrag.timer = setInterval(function () {
+      desX *= 0.95;
+      desY *= 0.95;
+      tX += desX * 0.1;
+      tY += desY * 0.1;
+      applyTranform(odrag);
+      playSpin(false);
+      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+        clearInterval(odrag.timer);
+        playSpin(true);
+      }
+    }, 17);
+    this.onpointermove = this.onpointerup = null;
+  };
+
+  return false;
+};
+
+document.onmousewheel = function(e) {
+  e = e || window.event;
+  var d = e.wheelDelta / 20 || -e.detail;
+  radius += d;
+  init(1);
+};
+// Your existing JavaScript for the 3D carousel
+// ...
+
+// Fireworks JavaScript
+const PI2 = Math.PI * 2;
+const random = (min, max) => Math.random() * (max - min + 1) + min | 0;
+const timestamp = _ => new Date().getTime();
+
+class Birthday {
+    constructor() {
+        this.resize();
+        this.fireworks = [];
+        this.counter = 0;
+    }
+
+    resize() {
+        this.width = canvas.width = window.innerWidth;
+        let center = this.width / 2 | 0;
+        this.spawnA = center - center / 4 | 0;
+        this.spawnB = center + center / 4 | 0;
+
+        this.height = canvas.height = window.innerHeight;
+        this.spawnC = this.height * .1;
+        this.spawnD = this.height * .5;
+    }
+
+    onClick(evt) {
+        let x = evt.clientX || evt.touches && evt.touches[0].pageX;
+        let y = evt.clientY || evt.touches && evt.touches[0].pageY;
+
+        let count = random(3, 10);
+        for (let i = 0; i < count; i++) this.fireworks.push(new Firework(
+            random(this.spawnA, this.spawnB),
+            this.height,
+            x,
+            y,
+            random(0, 260),
+            random(30, 110)));
+
+        this.counter = -1;
+    }
+
+    update(delta) {
+        ctx.globalCompositeOperation = 'hard-light';
+        ctx.fillStyle = `rgba(20,20,20,${7 * delta})`;
+        ctx.fillRect(0, 0, this.width, this.height);
+
+        ctx.globalCompositeOperation = 'lighter';
+        for (let firework of this.fireworks) firework.update(delta);
+
+        this.counter += delta * 3;
+        if (this.counter >= 1) {
+            this.fireworks.push(new Firework(
+                random(this.spawnA, this.spawnB),
+                this.height,
+                random(0, this.width),
+                random(this.spawnC, this.spawnD),
+                random(0, 360),
+                random(30, 110)));
+            this.counter = 0;
+        }
+
+        if (this.fireworks.length > 1000) this.fireworks = this.fireworks.filter(firework => !firework.dead);
+    }
+}
+
+class Firework {
+    constructor(x, y, targetX, targetY, shade, offsprings) {
+        this.dead = false;
+        this.offsprings = offsprings;
+        this.x = x;
+        this.y = y;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.shade = shade;
+        this.history = [];
+    }
+
+    update(delta) {
+        if (this.dead) return;
+
+        let xDiff = this.targetX - this.x;
+        let yDiff = this.targetY - this.y;
+        if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
+            this.x += xDiff * 2 * delta;
+            this.y += yDiff * 2 * delta;
+
+            this.history.push({ x: this.x, y: this.y });
+
+            if (this.history.length > 20) this.history.shift();
+        } else {
+            if (this.offsprings && !this.madeChilds) {
+                let babies = this.offsprings / 2;
+                for (let i = 0; i < babies; i++) {
+                    let targetX = this.x + this.offsprings * Math.cos(PI2 * i / babies) | 0;
+                    let targetY = this.y + this.offsprings * Math.sin(PI2 * i / babies) | 0;
+                    birthday.fireworks.push(new Firework(this.x, this.y, targetX, targetY, this.shade, 0));
+                }
+            }
+            this.madeChilds = true;
+            this.history.shift();
+        }
+
+        if (this.history.length === 0) this.dead = true;
+        else if (this.offsprings) {
+            for (let i = 0; this.history.length > i; i++) {
+                let point = this.history[i];
+                ctx.beginPath();
+                ctx.fillStyle = 'hsl(' + this.shade + ',100%,' + i + '%)';
+                ctx.arc(point.x, point.y, 1, 0, PI2, false);
+                ctx.fill();
+            }
+        } else {
+            ctx.beginPath();
+            ctx.fillStyle = 'hsl(' + this.shade + ',100%,50%)';
+            ctx.arc(this.x, this.y, 1, 0, PI2, false);
+            ctx.fill();
+        }
+    }
+}
+
+let canvas = document.getElementById('birthday');
+let ctx = canvas.getContext('2d');
+let then = timestamp();
+
+let birthday = new Birthday();
+window.onresize = () => birthday.resize();
+document.onclick = evt => birthday.onClick(evt);
+document.ontouchstart = evt => birthday.onClick(evt);
+
+(function loop() {
+    requestAnimationFrame(loop);
+    let now = timestamp();
+    let delta = now - then;
+    then = now;
+    birthday.update(delta / 1000);
+})();
