0dc281fdf74b59e6d06434e25d0c8edfcf506e0e
khushigavate <error404khushi@gmail.com>
2024-07-17 19:13:06 +0530

    Update sc.js

--- sc.js
+++ sc.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "07/17/",
+        dayMonth = "07/22/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
